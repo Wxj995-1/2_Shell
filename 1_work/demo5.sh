b514594695980bb4c77c880635d2ab7976081ab7
Ubuntu995 <za00011@163.com>
2026-03-24 10:35:54 +0800

    update

--- 1_work/demo5.sh
+++ 1_work/demo5.sh
@@ -1,1 +1,13 @@
 #! /bin/bash  
+#echo "${PATH}:hello world"
+
+#echo "\"${PATH}\":hello world"
+
+#echo "${#PATH}"
+
+#字符串拼接
+#echo ${Weng2}${PATH}
+#echo ${Weng2}'0000'${PATH}
+#echo ${Weng2}"5555"${PATH}
+
+echo "${HOME}  hello world"
